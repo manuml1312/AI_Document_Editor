a4cbdbaf5fac670074a31b59d1abde5985bac2ad
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-09-12 15:57:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from docx import Document
 
 # from openai import OpenAI
+your_api_key=st.secrets.api_key
+client=OpenAI(api_key=your_api_key)
 
 edit_styles=['Basic','Standard','Pro']
 # Streamlit UI
@@ -20,5 +22,15 @@ if st.button('Edit Text'):
   text=''
   for i in document.paragraphs:
     text+=i.text
-  st.write(text)
+  prompt=f'Correct any grammatical issues in the provided text.The provided text is:{text}'
+  res = client.chat.completions.create(
+      model="gpt-3.5-turbo-0125",  # Choose the appropriate OpenAI GPT engine
+      max_tokens=1028,
+       messages=[
+        {"role": "system", "content": """You are AI Assistant.Do as asked."""},
+      {"role": "user", "content": str(prompt)}])
+  response=dict(dict([dict(res)][0]['choices'][0])['message'])['content']
+  st.write(response)
+
+
 
