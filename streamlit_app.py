10782d1a523f92398c4a8d01cad78b97db0a8605
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-09-12 15:37:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+edit_styles=['Basic','Standard','Pro']
+# Streamlit UI
+st.title("Text Editor")
+
+# if st.button('Generate'):
+style = st.selectbox("Select the style of the tattoo", edit_styles)
+
+# if st.button("Edit Image"):
+uploaded_file=st.file_uploader("Upload the text document to process.",type=["docx","pdf","csv"],key='orig')
+
+edits = st.multiselect('Select three known variables:',['A','B','C'])
+
+st.button('Edit Text')
