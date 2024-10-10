38f25e002f862f5ae8bb85cbd1d54c13170e9925
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-10-10 10:23:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,9 @@ def save_instructions(data, filename):
 def read_docx(file_path):
     """ Extract text from DOCX file. """
     doc = Document(file_path)
-    return "\n".join([paragraph.text for paragraph in doc.paragraphs])
+    # return "\n".join([paragraph.text for paragraph in doc.paragraphs])
+    text=[paragraph.text for paragraph in doc.paragraphs]
+    return text
 
 def process_text_with_api(groups, instructions):
     context=''
@@ -82,7 +84,7 @@ def process_text_with_api(groups, instructions):
         data_sum = {
             'model':'gpt-4-turbo',
             'messages':messages_sum,
-            'max_tokens':250
+            'max_tokens':
         }
         ############################################
         response = requests.post(API_URL, headers=headers, json=data)
@@ -103,10 +105,10 @@ def process_text_with_api(groups, instructions):
 def process_document(filename, options,report_features,edits):
     """ Read the DOCX file, process the text with loaded instructions and additional features, call the API. """
     text = read_docx(filename)
-    groups=text_break(text)
+    # groups=text_break(text)
     instructions = load_instructions(options)
     combined_text = instructions + " " + " ".join([report_features[feature] for feature in edits])
-    return process_text_with_api(groups, combined_text) 
+    return process_text_with_api(text, combined_text) 
 
 def create_docx(text):
     doc = Document()
