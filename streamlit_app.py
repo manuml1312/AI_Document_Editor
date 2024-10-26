1c5c4dede9ee155474d172518e0556a31336971e
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-10-26 19:21:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ def process_text_with_api(groups, instructions):
                 'model': 'gpt-4o',
                 'messages':messages,
                 'max_tokens': 6000,
-                'temperature': 0.75,
+                'temperature': 0.7,
                 'top_p': 1,
                 'frequency_penalty': 0,
                 'presence_penalty': 0
